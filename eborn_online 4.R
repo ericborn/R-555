0648ee548fcca0e61b12931ccd727f1157b575e1
Eric Born <eric.born85@gmail.com>
2019-08-04 09:54:42 -0700

    Add files via upload

--- eborn_online 4.R
+++ eborn_online 4.R
@@ -17,3 +17,30 @@ cor(dat$Score, dat$Income)
 # -0.006312 
 m <- lm(dat$Education ~ dat$Score)
 summary(m)
+
+# 4)
+# Calculate the least squares regression equation that predicts prestige score from education, 
+# income and percentage of women. What is the regression coefficient (beta) for the income?
+# -0.0003433
+mlr.fit <- lm(Score ~ Education + Income + WorkforceWomen)
+summary(mlr.fit)
+
+# 5)
+# Calculate the least squares regression equation that predicts prestige score 
+# from education, income and percentage of women. Formally test whether the set of 
+# these predictors are associated with prestige at the alpha= 0.05 level. What is the F-statistic?
+# 0.407
+summary(mlr.fit)
+
+# 6)
+# Calculate the least squares regression equation that predicts prestige score from 
+# education, income and percentage of women. What is r-squared value for least squares 
+# regression ? (Enter the Adjusted R-squared)
+# -0.01793
+summary(mlr.fit)
+
+# Calculate the least squares regression equation that predicts prestige score from education, 
+# income and percentage of women. Formally test whether income is a significant predictor 
+# associated with prestige score at the alpha= 0.05 level. What is the t-statistic for income?
+# -0.867
+summary(mlr.fit)
