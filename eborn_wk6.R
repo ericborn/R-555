ac3fe5b92e4418f38500a539668ba7d830b87016
Eric Born <eric.born85@gmail.com>
2019-08-14 13:39:55 -0700

    Add files via upload

--- eborn_wk6.R
+++ eborn_wk6.R
@@ -7,7 +7,7 @@ library(plotly)
 dat <- read.csv(file='c:/CS_555_Assignment_6.csv', sep = ',')
 
 # creates a new column temp_level
-# 1 if body temp >= 98.6, 0 if below < 98.6
+# 0 if below < 98.6, 1 if body temp >= 98.6
 dat$temp_level <- ifelse(dat$temp >= 98.6, 1, 0)
 
 # create comparison table between temp level and sex
@@ -17,18 +17,41 @@ tab <- table(dat$temp_level, dat$sex)
 tab
 
 # 2)
-m <- glm(dat$temp_level ~ dat$sex, family = "binomial")
+# total male less than 98.6 / total males
+prop.male   <- tab[2,1]/colSums(tab)[1]
 
-# predict risk for each patient
-risk <-predict(m, type=c("response"))
+# total female less than 98.6 / total females
+prop.female <- tab[2,2]/colSums(tab)[2]
 
-risk 
+# male risk minus female risk
+risk.diff <- prop.male - prop.female
+
+# test proportions
+# male/female above 98.6 as x, total male/female as y
+prop.test(x=tab['1',], n=colSums(tab), conf.level=0.95, correct=FALSE)
+
+# 3)
+# create dummy variables female(2) = 1, male(1) = 0
+dat$s0 <- ifelse(dat$sex==2, 1, 0)
+
+# What is the regression coefficient for girls in your logistic regression?
+
+# logisitic regression with temp_level as the response
+# and sex as the predictor or explanatory variable.
+m <- glm(temp_level ~ sex, data = dat, family = 'binomial')
+
+# create probabilities
+dat$prob <- predict(m, type=c("response"))
+
+summary(m)
 
 
-# setup proportions for male and above/below body temp
-prop.male.dat   <- tab[2,1]/colSums(tab)[1]
-prop.female.dat <- tab[2,2]/colSums(tab)[2]
+prop.test (70 , 100 , p=0.8 , conf.level=0.95 , correct=FALSE)
+# What is the c-statistic (Area under ROC curve) of this model?
 
-# Take the absoluate value from male minus female high body temp
-# 43
-abs(prop.male.dat-prop.female.dat)
+
+# ROC Curve 
+g <- roc(dat$temp_level ~ dat$prob)
+
+# 0.7167
+g
