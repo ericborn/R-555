00b0487849dcf71fbd8157ee66630a49a957e379
Eric Born <eric.born85@gmail.com>
2019-08-14 15:46:51 -0700

    Add files via upload

--- eborn_wk6.R
+++ eborn_wk6.R
@@ -1,5 +1,7 @@
 #library(ggplot2)
 library(plotly)
+install.packages('oddsratio')
+library(oddsratio)
 
 # 1)
 # Read the data into R
@@ -43,15 +45,37 @@ m <- glm(temp_level ~ sex, data = dat, family = 'binomial')
 # create probabilities
 dat$prob <- predict(m, type=c("response"))
 
-summary(m)
+# calculate odds ratio
+exp(cbind(OR = coef(m), confint(m, level = 0.95)))
 
-
-prop.test (70 , 100 , p=0.8 , conf.level=0.95 , correct=FALSE)
 # What is the c-statistic (Area under ROC curve) of this model?
 
-
-# ROC Curve 
+# ROC curve 
 g <- roc(dat$temp_level ~ dat$prob)
 
 # 0.7167
 g
+
+# Plot the ROC curve
+plot(g)
+
+# 4)
+# Multiple logistic regression
+m2 <- glm(temp_level ~ sex + heartRate, data = dat, family = 'binomial')
+
+summary(m2)
+
+# calculate odds ratio
+exp(cbind(OR = coef(m2), confint(m2, level = 0.95))*10)
+
+# create probabilities
+dat$prob2 <- predict(m2, type=c("response"))
+
+# ROC curve 
+g2 <- roc(dat$temp_level ~ dat$prob2)
+
+# 0.7167
+g2
+
+# Plot the ROC curve
+plot(g2)
